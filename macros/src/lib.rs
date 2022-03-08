9bff114a0ca43dbb73e4c87d1ee1e36203cc7308
Patrick White <1275985+packysauce@users.noreply.github.com>
2022-03-08 10:19:12 -0800

    make assocs from traits?

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,14 +1,14 @@
-use darling::FromDeriveInput;
+use darling::{FromDeriveInput, FromMeta};
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, AttributeArgs, DeriveInput};
 
 mod helpers;
 
 #[proc_macro_derive(Entity, attributes(entity))]
 pub fn make_entity_macro(tokens: TokenStream) -> TokenStream {
     let input = parse_macro_input!(tokens as DeriveInput);
-    let stuff = helpers::Entity::from_derive_input(&input).unwrap();
+    let stuff = helpers::EntityDeriveInput::from_derive_input(&input).unwrap();
     let t = quote!(#stuff);
     t.into()
 }
@@ -16,7 +16,37 @@ pub fn make_entity_macro(tokens: TokenStream) -> TokenStream {
 #[proc_macro_derive(Assoc, attributes(assoc))]
 pub fn make_assoc_macro(tokens: TokenStream) -> TokenStream {
     let input = parse_macro_input!(tokens as DeriveInput);
-    let stuff = helpers::Assoc::from_derive_input(&input).unwrap();
+    let stuff = helpers::AssocDeriveInput::from_derive_input(&input).unwrap();
     let t = quote!(#stuff);
     t.into()
+}
+
+#[derive(FromMeta)]
+struct IdAttribute {
+    pub id: u64,
+}
+
+#[proc_macro_attribute]
+pub fn assoc(args: TokenStream, input: TokenStream) -> TokenStream {
+    let attrs = parse_macro_input!(args as AttributeArgs);
+    let item = parse_macro_input!(input as syn::ItemTrait);
+    let args = match IdAttribute::from_list(&attrs) {
+        Ok(v) => v,
+        Err(e) => return TokenStream::from(e.write_errors()),
+    };
+
+    let (impl_generics, ty_generics, where_clause) = item.generics.split_for_impl();
+    let name = &item.ident;
+    let id = args.id;
+
+    let impl_def = quote! {
+        impl #impl_generics ::wtf::AssocTypeID for dyn #name #ty_generics
+        #where_clause
+        {
+            const TYPE_ID: u64 = #id;
+        }
+
+        #item
+    };
+    impl_def.into()
 }
