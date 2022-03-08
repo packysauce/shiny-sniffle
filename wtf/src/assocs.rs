d6a2d6c4de2e609a95964d8aa90e7e3c2db5e6bd
Patrick White <1275985+packysauce@users.noreply.github.com>
2022-03-08 10:46:33 -0800

    get rid of Raw* types

--- wtf/src/assocs.rs
+++ wtf/src/assocs.rs
@@ -1,7 +1,7 @@
 use tea::TeaConnection;
 
 use crate::{
-    entities::{EntityTypeID, RawEntity},
+    entities::EntityTypeID,
     state::{Dirty, SaveResult},
     Ent, PersistedState, SaveError, Saved,
 };
@@ -52,19 +52,6 @@ where
             kind,
         }
     }
-}
-
-/// Storage of an assocation. If you think of an assocation as an arrow,
-/// then the base of the arrow is the "from" entity, and the "to" entity
-/// is being pointed at by the arrow.
-#[derive(Debug, Clone, Copy, PartialEq)]
-struct RawAssoc {
-    // In TAO parlance, id1
-    pub from: RawEntity,
-    // In TAO parlance, id2
-    pub to: RawEntity,
-    // In TAO parlance, atype
-    ty: AssocType,
 }
 
 impl<A, F, T> Assoc<A, F, T, Dirty>
--- wtf/src/entities.rs
+++ wtf/src/entities.rs
@@ -22,40 +22,15 @@ impl<T: EntityTypeID> Ent<T> {
     }
 }
 
-/// An entity, at the atomic level.
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct RawEntity {
-    // alright fine, quarks here
-    /// the entity's ID - this is globally unique
-    id: EntityId, // i have 100% thought of this as subnet
-    ty: TypeId, // and host and you bet your fuckin biscuits i will try it out
-}
-
-impl RawEntity {
-    pub fn new(id: EntityId, ty: TypeId) -> Self {
-        Self { id, ty }
-    }
-
-    /// Get a reference to the raw entity's id.
-    pub fn id(&self) -> EntityId {
-        self.id
-    }
-
-    /// Get a reference to the raw entity's ty.
-    pub fn ty(&self) -> TypeId {
-        self.ty
-    }
-}
-
 pub trait Save
 where
     Self: Sized + EntityTypeID,
 {
-    fn save(self, db: &mut dyn TeaConnection) -> SaveResult<Ent<Self>>;
+    fn save<DB: TeaConnection>(self, db: &mut DB) -> SaveResult<Ent<Self>>;
 }
 
 impl<T: EntityTypeID + Serialize> Save for T {
-    fn save(self, db: &mut dyn TeaConnection) -> SaveResult<Ent<Self>> {
+    fn save<DB: TeaConnection>(self, db: &mut DB) -> SaveResult<Ent<Self>> {
         let data = serde_json::to_vec(&self).map_err(SaveError::from)?;
         let ty = EntityType::from_u64(T::TYPE_ID).unwrap();
         let id = db.ent_add(ty, &data).map_err(SaveError::from)?;
