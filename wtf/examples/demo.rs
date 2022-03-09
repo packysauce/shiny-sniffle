9b58701dafe12a2595e1f5ae451583749ab044c8
Patrick White <1275985+packysauce@users.noreply.github.com>
2022-03-08 22:27:51 -0800

    what the thing should poop out

--- wtf/examples/demo.rs
+++ wtf/examples/demo.rs
@@ -1,16 +1,49 @@
 use rusqlite::DatabaseName;
 use serde::{Deserialize, Serialize};
-use wtf::{Ent, SaveEnt, TeaConnection};
+use wtf::{Assoc, AssocTypeID, Ent, EntityTypeID, SaveEnt, TeaConnection};
 use wtf_macros::Entity;
 
-#[wtf_macros::assoc(id = 1)]
-pub trait Authored<F, T>
+pub struct Author;
+pub type AuthorAssoc<'f, 't, Id1, Id2> = Assoc<'f, 't, Id1, Author, Id2>;
+
+impl AssocTypeID for Author {
+    const TYPE_ID: u64 = 1;
+}
+
+pub trait Authored<'a, Id1, Id2>: EntityTypeID + Sized
 where
-    T: ::wtf::EntityTypeID,
-    F: ::wtf::EntityTypeID,
+    Id1: EntityTypeID,
+    Id2: EntityTypeID,
 {
-    fn authored_by(&self, who: &Ent<T>);
-    fn authored(&self, what: &Ent<T>);
+    fn authored(&'a self, other: &'a Ent<Id2>) -> AuthorAssoc<'a, '_, Id1, Id2>;
+}
+
+impl<'a, Id1, Id2> Authored<'a, Id1, Id2> for Ent<Id1>
+where
+    Id1: EntityTypeID + 'a,
+    Id2: EntityTypeID + 'a,
+{
+    fn authored(&'a self, other: &'a Ent<Id2>) -> Assoc<'a, '_, Id1, Author, Id2> {
+        Assoc::new(self, other)
+    }
+}
+
+pub trait AuthoredBy<'a, Id1, Id2>: EntityTypeID + Sized
+where
+    Id1: EntityTypeID,
+    Id2: EntityTypeID,
+{
+    fn authored_by(&'a self, other: &'a Ent<Id2>) -> Assoc<'a, '_, Id1, Author, Id2>;
+}
+
+impl<'a, Id1, Id2> AuthoredBy<'a, Id1, Id2> for Ent<Id1>
+where
+    Id1: EntityTypeID + 'a,
+    Id2: EntityTypeID + 'a,
+{
+    fn authored_by(&'a self, other: &'a Ent<Id2>) -> Assoc<'a, '_, Id1, Author, Id2> {
+        Assoc::new(self, other)
+    }
 }
 
 #[derive(Entity, Debug, Serialize, Deserialize)]
@@ -87,11 +120,11 @@ fn main() -> anyhow::Result<()> {
     .save(&mut db)?;
 
     // set all them assocs up
-    let comment_author = comment.authored_by(&person).save(&mut db)?;
+    let comment_author = person.authored(&comment).save(&mut db)?;
     let play_author = play.authored_by(&person).save(&mut db)?;
     let book_author = book.authored_by(&person).save(&mut db)?;
 
-    assert!(comment_author == play_author && play_author == book_author);
+    //assert!(comment_author == play_author && play_author == book_author);
     db.backup(DatabaseName::Main, "thingy.sqlite", None)
         .unwrap();
     Ok(())
